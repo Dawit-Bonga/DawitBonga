9ec6209ad1d39be9a3871e4a32e9b973c89bfb1e
Dawit Bonga <Dawitbonga0@gmail.com>
2026-01-09 14:11:34 -0800

    added more experiences and format changes

--- lib/experience.ts
+++ lib/experience.ts
@@ -10,17 +10,7 @@ export interface WorkExperience {
 }
 
 const experiences: WorkExperience[] = [
-  {
-    title: 'Student Technician',
-    company: 'Yale Student Collabrative',
-    date: 'Jan 2024 – Present',
-    description: [
-      'Completed hands-on training program in troubleshooting and repairing computer hardware, gaining proficiency in diagnosing issues with a variety of devices.',
-      'Supported 12,000+ Yale students, resolving 25+ issues weekly in Windows, macOS, and Linux systems, including hardware diagnostics, networking, and OS repair.',
-    ],
-    technologies: ['Windows/macOS/Linux', 'Hardware Diagnostics', 'Network Troubleshooting', 'IT Support'],
-  },
-  {
+    {
     title: 'Software Engineering Intern',
     company: 'Amazon AWS',
     date: 'May 2025 – Aug 2025',
@@ -31,12 +21,35 @@ const experiences: WorkExperience[] = [
     ],
     technologies: ['AWS Lambda', 'AWS Fargate', 'Amazon Bedrock', 'CI/CD', 'Python', 'Java'],
   },
+    {
+    title: 'Undergraduate Research Assistant',
+    company: 'Yale Interactive Machines Group',
+    date: 'Dec 2025 – Present',
+    description: [
+      'Selected for NSF-funded research to deploy the Shutter social robot at the Yale Peabody Museum, managing full-lifecycle software updates.',
+      'Optimizing real-time motor control in C++ and implementing social interaction logic in Python/Pytrees.',
+      'Migrating robot infrastructure from legacy OS to Ubuntu 24.04, ensuring system stability for continuous public interaction',
+    ],
+    technologies: ['Python', 'C++', 'Ubunto', 'Robot Operating System'],
+  },
+  {
+    title: 'Student Technician',
+    company: 'Yale Student Collabrative',
+    date: 'Oct 2024 – Present',
+    description: [
+      'Completed hands-on training program in troubleshooting and repairing computer hardware, gaining proficiency in diagnosing issues with a variety of devices.',
+      'Supported 12,000+ Yale students, resolving 25+ issues weekly in Windows, macOS, and Linux systems, including hardware diagnostics, networking, and OS repair.',
+    ],
+    technologies: ['Windows/macOS/Linux', 'Hardware Diagnostics', 'Network Troubleshooting', 'IT Support'],
+  },
 ]
 
 export function getWorkExperience(): WorkExperience[] {
-  return experiences.sort((a, b) => {
-    // Sort by date, most recent first (simple string comparison for now)
-    return b.date.localeCompare(a.date)
-  })
+  // return experiences.sort((a, b) => {
+  //   // Sort by date, most recent first (simple string comparison for now)
+  //   return b.date.localeCompare(a.date)
+  // })
+
+  return experiences;
 }
 
